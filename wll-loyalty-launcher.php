18831c1b12b858e8c97e3b77b478113080f60cd6
Alagesan <alagesanbe08@gmail.com>
2025-05-06 13:35:11 +0530

    - version 1.0.2

--- wll-loyalty-launcher.php
+++ wll-loyalty-launcher.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPLoyalty - Launcher
  * Plugin URI: https://www.wployalty.net
  * Description: Launcher widget for WPLoyalty. Let your customers easily discover your loyalty rewards.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Wployalty
  * Slug: wll-loyalty-launcher
  * Text Domain: wll-loyalty-launcher
@@ -11,7 +11,7 @@
  * Requires Plugins: woocommerce
  * Requires at least: 4.9.0
  * WC requires at least: 6.5
- * WC tested up to: 9.7
+ * WC tested up to: 9.8
  * Author URI: https://wployalty.net/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -90,7 +90,7 @@ defined( 'WLL_MINIMUM_PHP_VERSION' ) or define( 'WLL_MINIMUM_PHP_VERSION', '7.4.
 defined( 'WLL_MINIMUM_WP_VERSION' ) or define( 'WLL_MINIMUM_WP_VERSION', '4.9' );
 defined( 'WLL_MINIMUM_WC_VERSION' ) or define( 'WLL_MINIMUM_WC_VERSION', '6.0' );
 defined( 'WLL_MINIMUM_WLR_VERSION' ) or define( 'WLL_MINIMUM_WLR_VERSION', '1.3.0' );
-defined( 'WLL_PLUGIN_VERSION' ) or define( 'WLL_PLUGIN_VERSION', '1.0.1' );
+defined( 'WLL_PLUGIN_VERSION' ) or define( 'WLL_PLUGIN_VERSION', '1.0.2' );
 defined( 'WLL_PLUGIN_SLUG' ) or define( 'WLL_PLUGIN_SLUG', 'wll-loyalty-launcher' );
 defined( 'WLL_PLUGIN_FILE' ) or define( 'WLL_PLUGIN_FILE', __FILE__ );
 defined( 'WLL_PLUGIN_DIR' ) or define( 'WLL_PLUGIN_DIR', str_replace( '\\', '/', __DIR__ ) );
